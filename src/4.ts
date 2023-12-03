a591a35fa2f9e7f06695274a1df51ff5d6dc918d
Oleg Bilas <olegbilas@gmail.com>
2023-12-03 13:52:44 +0200

    finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+
+class Key{
+    private signature: number = Math.random();
+    
+    //  constructor() {
+    //     this.signature = Math.random();
+    //  }
+    getSignature() {
+        return this.signature;
+    }
+    
+}
+
+class Person{
+    constructor(private key: Key) {
+        
+    }
+
+    getKey() {
+        return this.key;
+    }
+}
+
+
+abstract class House{
+    protected door: true | false;
+    tenants: Person[];
+
+    constructor(protected key: Key) { 
+        this.door = false;
+    }
+
+    comeIn (person:Person){
+        if (this.door === true) {
+    this.tenants.push(person);
+        }
+        console.log("Come in");
+        
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+
+class MyHouse extends House{
+
+    constructor(key: Key) { 
+        super(key);
+    }
+    openDoor(key: Key){
+    
+    if (this.key === key) {
+        this.door = true;
+        }
+    };
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
